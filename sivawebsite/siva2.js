ea4b4d8682ec123fbd6eceddc8660ba4dc5f203e
spartantrap17O3 <bft1208@outlook.com>
2023-05-09 16:41:40 -0700

    Error icons fixed

--- sivawebsite/siva2.js
+++ sivawebsite/siva2.js
@@ -11,9 +11,8 @@ function closeNav() {
 }
 
 var coll = document.getElementsByClassName("collapsible");
-var i;
 
-for (i = 0; i < coll.length; i++) {
+for (var i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
